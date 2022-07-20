4a91ce005bac8cb1c4a4ab0ce3e9dbcb18b46de0
Francisco Leal <franciscoleal152000@gmail.com>
2022-07-20 20:01:04 -0300

    fetch

--- main.js
+++ main.js
@@ -276,3 +276,26 @@ document.getElementById("ingresar").addEventListener("click",function(){
 
     }
 })
+
+document.getElementById("boton5").addEventListener("click",function(){
+    const lista = document.querySelector('#listado')
+
+    fetch('/data.json')
+        .then( (res) => res.json())
+        .then( (data) => {
+
+            data.forEach((producto) => {
+                const li = document.createElement('li')
+                li.innerHTML = `
+                    <h4>${producto.nombre}</h4>
+                    <p>${producto.precio}</p>
+                    <p>Código: ${producto.id}</p>
+                    <hr/>
+                `
+   
+            lista.append(li)
+        })
+    })
+
+    
+})
